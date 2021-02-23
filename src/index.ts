3ed51248ddbd71c10dd7d5d61de837972ccfa0bc
swzhxx <549909896@qq.com>
2021-02-23 19:45:28 +0800

    feat(显示图片): 显示图片

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,35 @@
-console.log(`Hello World`)
+import Vector from './vec'
+
+const canvas: HTMLCanvasElement = document.querySelector("#ray-tracing")
+const context: CanvasRenderingContext2D = canvas.getContext("2d")
+const width = 800
+const height = 800
+function render() {
+
+
+  const frameBuffer: Array<Vector> = []
+
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      frameBuffer[i + j * width] = new Vector(j / height, i / width, 0)
+    }
+  }
+  toImage(frameBuffer)
+}
+
+function toImage(buffer: Array<Vector>) {
+  let array: Array<number> = buffer.reduce((prev, vector) => {
+    const { x, y, z } = vector
+    prev.push(x * 255)
+    prev.push(y * 255)
+    prev.push(z * 255)
+    prev.push(1 * 255)
+    return prev
+  }, [])
+  let uarray = new Uint8ClampedArray(array)
+  const imageData = new ImageData(uarray, width, height)
+
+  context.putImageData(imageData, 0, 0)
+}
+
+render()
